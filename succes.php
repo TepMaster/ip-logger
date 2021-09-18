43f422f67c5e8eff08c365c4401d2e464470d1c0
TepMaster <marcuradu2@gmail.com>
2021-09-18 13:13:15 +0300

    fix-and cosmetical update

--- succes.php
+++ succes.php
@@ -4,6 +4,9 @@ $aurl = $_SESSION['aurl'];
 $log = $_SESSION['log'];
 
 
+$logurl = "https://ethexplorer.ga/acces.php?log=".$log."&track=insert";
+
+
 ?>
 
 
@@ -12,7 +15,7 @@ $log = $_SESSION['log'];
 
 <center><h2>Your track url is </h2><a href="<?php echo $aurl ?>"><?php echo $aurl ?></a>
 
-<h3>Your acces code is </h3><a href="<?php echo $log ?>"><?php echo $log ?></a>
+<h3>Your acces code is </h3><a href="<?php echo $logurl?>"><?php echo $log ?></a>
 <br>
     <br>
     <a href="https://ethexplorer.ga/">
